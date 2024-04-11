5aee375fdc559420bf22a339f64884c44580072d
devopsdevopsaws <abilashdevopsaws@gmail.com>
2024-04-11 15:40:12 +0530

    commited

--- peering.tf
+++ peering.tf
@@ -23,21 +23,21 @@ resource "aws_route" "default_route" {
 
 resource "aws_route" "public_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route.public.id
+  route_table_id            = aws_route_table.public.id
   destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "private_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route.private.id
+  route_table_id            = aws_route_table.private.id
   destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
 
 resource "aws_route" "database_peering" {
   count = var.is_peering_required ? 1 : 0
-  route_table_id            = aws_route.database.id
+  route_table_id            = aws_route_table.database.id
   destination_cidr_block    = var.default_vpc_cidr
   vpc_peering_connection_id = aws_vpc_peering_connection.peering[0].id
 }
